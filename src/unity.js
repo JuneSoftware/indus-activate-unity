7bee51794fcc46c8f3dadb909e518e64f0b7862c
Valiant John <valiant@supergaming.com>
2023-03-17 00:43:18 +0530

    Updated workflow

--- src/unity.js
+++ src/unity.js
@@ -5,9 +5,6 @@ module.exports = { createManualActivationFile, activateManualLicense, activateSe
 
 async function activateSerialLicense(unityPath, username, password, serial) {
     const stdout = await executeUnity(unityPath, `-batchmode -nographics -username "${username}" -password "${password}" -serial "${serial}" -quit`);
-    if (!stdout.includes('Successfully activated ULF license')) {
-        throw new Error('Activation failed');
-    }
 }
 
 async function createManualActivationFile(unityPath) {
